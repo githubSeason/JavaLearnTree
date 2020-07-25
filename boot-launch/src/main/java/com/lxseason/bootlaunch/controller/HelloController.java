cb99bedd8ac2c49fda404d7f68813822604e6861
githubSeason <1936206520@qq.com>
2020-07-25 22:05:54 +0800

    add springboot hello world module

---
+++ boot-launch/src/main/java/com/lxseason/bootlaunch/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.lxseason.bootlaunch.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hellp(){
+        return "hello spring boot";
+    }
+}
